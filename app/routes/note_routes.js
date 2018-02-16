45b63e9213872225875e5116878ff8385e8c9a28
Aleksey Gribov <gribov_an@open.ru>
2018-02-16 14:47:02 +0300

    FIX обновление процедуры добавления новой записи

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,49 +1,7 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function (app, db) {
-    // app.get('/notes/:id', (req, res) => {
-    //     const id = req.params.id;
-    //     const details = { '_id': new ObjectID(id) };
-    //     db.collection('notes').findOne(details, (err, item) => {
-    //         if (err) {
-    //             res.send({ 'error': 'An error has occurred' });
-    //         } else {
-    //             res.send(item);
-    //         }
-    //     });
-    // });
-    // app.post('/notes', (req, res) => {
-    //     const note = { text: req.body.body, title: req.body.text };
-    //     db.collection('notes').insert(note, (err, result) => {
-    //         if (err) {
-    //             res.send({ 'error': 'An error has occurred' });
-    //         } else {
-    //             res.send(result.ops[0]);
-    //         }
-    //     });
-    // });
-    // app.delete('/notes/:id', (req, res) => {
-    //     const id = req.params.id;
-    //     const details = { '_id': new ObjectID(id) };
-    //     db.collection('notes').remove(details, (err, item) => {
-    //         if (err) {
-    //             res.send({ 'error': 'An error has occurred' });
-    //         } else {
-    //             res.send('Note ' + id + ' deleted!');
-    //         }
-    //     });
-    // });
-    // app.put('/notes/:id', (req, res) => {
-    //     const id = req.params.id;
-    //     const details = { '_id': new ObjectID(id) };
-    //     const note = { text: req.body.body, title: req.body.title };
-    //     db.collection('notes').update(details, note, (err, result) => {
-    //         if (err) {
-    //             res.send({ 'error': 'An error has occurred' });
-    //         } else {
-    //             res.send(note);
-    //         }
-    //     });
-    // });
+    
+    // добавить операцию
     app.post('/addOperation', (req, res) => {
         const operations = {
             amount: req.body.amount,
@@ -60,8 +18,8 @@ module.exports = function (app, db) {
         });
     });
 
+    // получить последние 5 операций
     app.post('/getLastFive', (req, res) => {
-        console.log('getLastTen');
         db.collection('operations').
             find({}).
             sort({ "_id": -1 }).
@@ -75,8 +33,8 @@ module.exports = function (app, db) {
             );
     });
 
+    // добавить счет
     app.post('/addAccount', (req, res) => {
-        console.log('addAccount');
         const account = {
             name: req.body.name,
             balance: req.body.balance,
@@ -92,8 +50,8 @@ module.exports = function (app, db) {
         });
     });
 
+    // получить список счетов
     app.post('/getAccounts', (req, res) => {
-        console.log('getAccounts');
         db.collection('accounts').
             find({}).
             toArray().
@@ -105,16 +63,23 @@ module.exports = function (app, db) {
             );
     });
 
-    // app.put('/updateAccount', (req, res) => {
-    app.post('/updateAccount', (req, res) => {
-        console.log('updateAccount');
-        console.log(req.body.id);
-        const type = (req.body.typeOperation === '0') ? '-' : '+';
-        // const id = req.params.id;
-        // const balance = req.params.accountBalance;
-        // const typeOperation = req.params.typeOperation;
+    // обновить сумму у счетов после создания операции
+    app.post('/updateAccountAmount', (req, res) => {
+        const details = { '_id': new ObjectID(req.body.accountId) };
+        const note = { balance: req.body.amount };
+        db.collection('accounts').update(details, {$set: note}, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(note);
+            }
+        });
+    });
+
+    // обновление названия и сумму счета(редактирование)
+    app.post('/EditAccount', (req, res) => {
         const details = { '_id': new ObjectID(req.body.id) };
-        const note = {balance: type + req.body.accountBalance };
+        const note = { balance: type + req.body.accountBalance };
         db.collection('accounts').update(details, note, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
