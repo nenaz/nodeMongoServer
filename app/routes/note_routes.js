20e4e4429ad55887c53d3d9a7a848b68249e479c
Aleksey Gribov <gribov_an@open.ru>
2018-03-22 10:41:03 +0300

    FIX

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,15 +1,50 @@
 var ObjectID = require('mongodb').ObjectID;
+
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('../../config/db');
+
+passport.use(new Strategy(
+    function (username, password, cb) {
+        db.users.findByUsername(username, function (err, user) {
+            if (err) { return cb(err); }
+            if (!user) { return cb(null, false); }
+            if (user.password != password) { return cb(null, false); }
+            return cb(null, user);
+        });
+    }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    db.users.findById(id, function (err, user) {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
 module.exports = function (app, db) {
-    
+    app.get('/', (req, res) => {
+        console.log('/')
+        res.send('hello');
+    })
     // добавить операцию
     app.post('/addOperation', (req, res) => {
         const operations = {
-            amount: req.body.balance,
+            amount: req.body.amount,
             currency: req.body.currency,
             data: req.body.data,
-            account: req.body.account,
-            operCoord: req.body.operCoord,
-            typeOperation: req.body.typeOperation
+            account: req.body.account
         };
         db.collection('operations').insert(operations, (err, result) => {
             if (err) {
@@ -39,10 +74,15 @@ module.exports = function (app, db) {
     app.post('/addAccount', (req, res) => {
         const account = {
             name: req.body.name,
-            balance: req.body.balance,
+            amount: req.body.amount,
             currency: req.body.currency,
-            pname: req.body.pname
+            pname: req.body.pname,
+            date: req.body.date,
+            number: req.body.number,
+            people: req.body.people,
+            id: req.body._id
         };
+        console.log(req.body);
         db.collection('accounts').insert(account, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
@@ -67,12 +107,12 @@ module.exports = function (app, db) {
 
     // обновить сумму у счетов после создания операции
     app.post('/updateAccountAmount', (req, res) => {
-        const details = { '_id': new ObjectID(req.body._id) };
-        const note = { balance: req.body.balance };
-        if (req.body.name) {
-            note.name = req.body.name;
-        }
-        db.collection('accounts').update(details, {$set: note}, (err, result) => {
+        const details = { '_id': new ObjectID(req.body.id) };
+        const note = {
+            amount: req.body.amount,
+        };
+        console.log(req.body)
+        db.collection('accounts').update(details, note, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
             } else {
@@ -82,10 +122,18 @@ module.exports = function (app, db) {
     });
 
     // обновление названия и сумму счета(редактирование)
-    app.post('/EditAccount', (req, res) => {
+    app.post('/editAccount', (req, res) => {
         const details = { '_id': new ObjectID(req.body.id) };
-        const note = { balance: type + req.body.accountBalance };
-        db.collection('accounts').update(details, note, (err, result) => {
+        const note = {
+            amount: req.body.amount,
+            name: req.body.name,
+            accountDate: req.body.accountDate,
+            accountNumber: req.body.accountNumber,
+            accountPeople: req.body.accountPeople,
+        };
+        console.log(note);
+        console.log(req.body.amount);
+        db.collection('accounts').update(details, { $set: note }, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
             } else {
@@ -93,4 +141,22 @@ module.exports = function (app, db) {
             }
         });
     });
+
+    app.post('/Logon', passport.authenticate('local'),
+        function (req, res) {
+            console.log(req.body);
+            const obj = {
+                result: true
+            }
+            res.send(obj);
+        });
+
+    app.get('/Logon', passport.authenticate('local'),
+        function (req, res) {
+            console.log(req.body);
+            const obj = {
+                result: true
+            }
+            res.send(obj);
+        });
 };
