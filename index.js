07f0ceae60a6a2514195630b1c1df0b896035e6e
Aleksey Gribov <gribov_an@open.ru>
2018-04-20 09:37:54 +0300

    FIX

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import { urlencoded, json } from "body-parser";
 import { url } from "./config/db";
-import cookieParser from 'cookie-parser'
+// import cookieParser from 'cookie-parser'
 import routes from './app/routes'
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(urlencoded({ extended: true }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
